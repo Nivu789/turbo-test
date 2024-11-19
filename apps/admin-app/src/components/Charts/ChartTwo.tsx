2584e41ab31e6093e1a49b83659cde05e29acb31
Nived <nivuyt789@gmail.com>
2024-11-19 10:55:56 +0530

    try this

--- apps/admin-app/src/components/Charts/ChartTwo.tsx
+++ apps/admin-app/src/components/Charts/ChartTwo.tsx
@@ -4,88 +4,89 @@ import { ApexOptions } from "apexcharts";
 import React from "react";
 import dynamic from "next/dynamic";
 
-const ReactApexChart = dynamic(() => import("react-apexcharts"), {
-  ssr: false,
-});
+// const ReactApexChart = dynamic(() => import("react-apexcharts"), {
+//   ssr: false,
+// });
 
-const options: ApexOptions = {
-  colors: ["#3C50E0", "#80CAEE"],
-  chart: {
-    fontFamily: "Satoshi, sans-serif",
-    type: "bar",
-    height: 335,
-    stacked: true,
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: false,
-    },
-  },
+// const options: ApexOptions = {
+//   colors: ["#3C50E0", "#80CAEE"],
+//   chart: {
+//     fontFamily: "Satoshi, sans-serif",
+//     type: "bar",
+//     height: 335,
+//     stacked: true,
+//     toolbar: {
+//       show: false,
+//     },
+//     zoom: {
+//       enabled: false,
+//     },
+//   },
 
-  responsive: [
-    {
-      breakpoint: 1536,
-      options: {
-        plotOptions: {
-          bar: {
-            borderRadius: 0,
-            columnWidth: "25%",
-          },
-        },
-      },
-    },
-  ],
-  plotOptions: {
-    bar: {
-      horizontal: false,
-      borderRadius: 0,
-      columnWidth: "25%",
-      borderRadiusApplication: "end",
-      borderRadiusWhenStacked: "last",
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
+//   responsive: [
+//     {
+//       breakpoint: 1536,
+//       options: {
+//         plotOptions: {
+//           bar: {
+//             borderRadius: 0,
+//             columnWidth: "25%",
+//           },
+//         },
+//       },
+//     },
+//   ],
+//   plotOptions: {
+//     bar: {
+//       horizontal: false,
+//       borderRadius: 0,
+//       columnWidth: "25%",
+//       borderRadiusApplication: "end",
+//       borderRadiusWhenStacked: "last",
+//     },
+//   },
+//   dataLabels: {
+//     enabled: false,
+//   },
 
-  xaxis: {
-    categories: ["M", "T", "W", "T", "F", "S", "S"],
-  },
-  legend: {
-    position: "top",
-    horizontalAlign: "left",
-    fontFamily: "Satoshi",
-    fontWeight: 500,
-    fontSize: "14px",
+//   xaxis: {
+//     categories: ["M", "T", "W", "T", "F", "S", "S"],
+//   },
+//   legend: {
+//     position: "top",
+//     horizontalAlign: "left",
+//     fontFamily: "Satoshi",
+//     fontWeight: 500,
+//     fontSize: "14px",
 
-    markers: {
-      radius: 99,
-    },
-  },
-  fill: {
-    opacity: 1,
-  },
-};
+//     markers: {
+//       radius: 99,
+//     },
+//   },
+//   fill: {
+//     opacity: 1,
+//   },
+// };
 
-interface ChartTwoState {
-  series: {
-    name: string;
-    data: number[];
-  }[];
-}
+// interface ChartTwoState {
+//   series: {
+//     name: string;
+//     data: number[];
+//   }[];
+// }
 
 const ChartTwo: React.FC = () => {
-  const series = [
-    {
-      name: "Sales",
-      data: [44, 55, 41, 67, 22, 43, 65],
-    },
-    {
-      name: "Revenue",
-      data: [13, 23, 20, 8, 13, 27, 15],
-    },
-  ];
+  
+  // const series = [
+  //   {
+  //     name: "Sales",
+  //     data: [44, 55, 41, 67, 22, 43, 65],
+  //   },
+  //   {
+  //     name: "Revenue",
+  //     data: [13, 23, 20, 8, 13, 27, 15],
+  //   },
+  // ];
 
   return (
     <div className="col-span-12 rounded-sm border border-stroke bg-white p-7.5 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
@@ -134,7 +135,7 @@ const ChartTwo: React.FC = () => {
       </div>
 
       <div>
-        <div id="chartTwo" className="-mb-9 -ml-5">
+        {/* <div id="chartTwo" className="-mb-9 -ml-5">
           <ReactApexChart
             options={options}
             series={series}
@@ -142,7 +143,7 @@ const ChartTwo: React.FC = () => {
             height={350}
             width={"100%"}
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );
